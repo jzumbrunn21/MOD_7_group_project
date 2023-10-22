e0a552ea022356ade65a56cb556d70d7be365423
Josh <jzumbrunn@yahoo.com>
2023-10-22 14:50:09 -0400

    date validators

--- app/forms/billing_form.py
+++ app/forms/billing_form.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
 from app.models import Service
+from datetime import date
 
+
+def exp_date_check(form, field):
+    today = date.today
+    if field.data < today:
+        raise ValidationError('Your card cannot be expired.')
 
 class BillingForm(FlaskForm):
     card_full_name = StringField('card_full_name', validators=[DataRequired(), Length(min=6, max=100, message='Full Name must be between 6 and 100 characters')])
     card_number = StringField('card_number', validators=[DataRequired(), Length(min=15, max=19, message='Card Number must be betwen 15 and 19 characters')])
     card_cvv = StringField('card_cvv', validators=[DataRequired(), Length(min=3, max=7, message='CVV must be between 3 and 7 characters')])
     card_zipcode = StringField('card_zipcode', validators=[DataRequired(), Length(min=5, max=15, message='Zipcode must be between 5 and 15 characters') ])
-    card_exp_date = DateField('exp_date', validators=[DataRequired()])
+    card_exp_date = DateField('exp_date', validators=[DataRequired(), exp_date_check])
--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateTimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from app.models import Booking
+from datetime import datetime
+
+def time_date_check(field, form):
+    today_and_now = datetime()
+
+    if field.data > today_and_now:
+        raise ValidationError('You must book for a date in the future')
+
 
 class BookingForm(FlaskForm):
     start_date_and_time = DateTimeField('start_date_and_time', validators=[DataRequired()])
