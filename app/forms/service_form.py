4835297fe2b91bf9e048dd59a372bc9378033f90
Josh <jzumbrunn@yahoo.com>
2023-10-25 09:56:10 -0400

    create and update vals and divs

--- app/forms/service_form.py
+++ app/forms/service_form.py
@@ -7,7 +7,7 @@ from app.models import Service, categories
 class ServiceForm(FlaskForm):
 
     service_title = StringField('title', validators=[DataRequired(), Length(min=6, max=50, message='Service Title must be between 6 and 50 characters')])
-    service_description = StringField('description', validators=[DataRequired(), Length(min=1, max=255, message='Service Description must be between 1 and 255 characters')])
+    service_description = StringField('description', validators=[DataRequired(), Length(min=1, max=2000, message='Service Description must be between 1 and 255 characters')])
     service_price = IntegerField('price', validators=[DataRequired(), NumberRange(min=1, message='Service Price cannot be $0')])
     service_length_est = IntegerField('length_est', validators=[DataRequired(), NumberRange(min=1, message='Service Estimate is a minimum 1 hour')])
     service_category = SelectField('category', choices=[('Lawn Service', 'Lawn Service'),('Cleaning','Cleaning'), ('Moving','Moving')]) #double check
--- react-app/src/components/CreateNewService/index.js
+++ react-app/src/components/CreateNewService/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createServiceThunk } from "../../store/services";
+import "./CreateNewService.css";
 
 const CreateNewService = () => {
   const dispatch = useDispatch();
@@ -20,20 +21,28 @@ const CreateNewService = () => {
     e.preventDefault();
 
     const newErrors = {};
-    if (!title) {
-      newErrors.title = "Title is required";
+    if (!title || title.length < 6 || title.length > 50) {
+      newErrors.title =
+        "Title is required and must be between 6 and 50 characters ";
     }
 
-    if (!description) {
-      newErrors.description = "Description is required";
+    if (!description || description.length < 1 || description.length > 2000) {
+      newErrors.description =
+        "Description is required and must be between 1 and 2000 characters ";
     }
-    if (!url) {
-      newErrors.url = "Image url is required";
+
+    const validUrlEndings = [".jpg", ".jpeg", ".png"];
+    if (
+      !url ||
+      !validUrlEndings.some((ending) => url.toLowerCase().endsWith(ending))
+    ) {
+      newErrors.url =
+        "Image url is required and must end in .jpg, .jpeg, or .png";
     }
-    if (!price) {
+    if (!price || price < 1) {
       newErrors.price = "Price is required";
     }
-    if (!lengthEstimate) {
+    if (!lengthEstimate || lengthEstimate < 1) {
       newErrors.lengthEstimate = "Length Estimate is required";
     }
     if (!category) {
@@ -71,67 +80,81 @@ const CreateNewService = () => {
     <div className="create-service-container">
       <h1>Create Your New Service</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          What's your service called?
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          {errors.title && <span className="error">{errors.title}</span>}
-        </label>
-        <label>
-          Service Description
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          {errors.description && (
-            <span className="error">{errors.description}</span>
-          )}
-        </label>
-        <label>
-          ImageUrl
-          <textarea value={url} onChange={(e) => setUrl(e.target.value)} />
-          {errors.url && <span className="error">{errors.url}</span>}
-        </label>
-        <label>
-          Price (per hour)
-          <input
-            type="number"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-          {errors.price && <span className="error">{errors.price}</span>}
-        </label>
-        <label>
-          Service Length Estimate
-          <input
-            type="number"
-            value={lengthEstimate}
-            onChange={(e) => setLengthEstimate(e.target.value)}
-          />
-          {errors.lengthEstimate && (
-            <span className="error">{errors.lengthEstimate}</span>
-          )}
-        </label>
-        <label>
-          Service Category
-          <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <option value="Lawn Service">Lawn Service</option>
-            <option value="Cleaning">Cleaning</option>
-            <option value="Moving">Moving</option>
-          </select>
-          {/* <input
+        <div className="create-service-title">
+          <label>
+            What's your service called?
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            {errors.title && <span className="error">{errors.title}</span>}
+          </label>
+        </div>
+        <div className="create-service-description">
+          <label>
+            Service Description
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            {errors.description && (
+              <span className="error">{errors.description}</span>
+            )}
+          </label>
+        </div>
+        <div className="create-service-url">
+          <label>
+            ImageUrl
+            <textarea value={url} onChange={(e) => setUrl(e.target.value)} />
+            {errors.url && <span className="error">{errors.url}</span>}
+          </label>
+        </div>
+        <div className="create-service-price">
+          <label>
+            Price (per hour)
+            <input
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            {errors.price && <span className="error">{errors.price}</span>}
+          </label>
+        </div>
+        <div className="create-service-length">
+          <label>
+            Service Length Estimate
+            <input
+              type="number"
+              value={lengthEstimate}
+              onChange={(e) => setLengthEstimate(e.target.value)}
+            />
+            {errors.lengthEstimate && (
+              <span className="error">{errors.lengthEstimate}</span>
+            )}
+          </label>
+        </div>
+        <div className="create-service-category">
+          <label>
+            Service Category
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="Lawn Service">Lawn Service</option>
+              <option value="Cleaning">Cleaning</option>
+              <option value="Moving">Moving</option>
+            </select>
+            {/* <input
             type="text"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
           /> */}
-          {errors.category && <span className="error">{errors.category}</span>}
-        </label>
+            {errors.category && (
+              <span className="error">{errors.category}</span>
+            )}
+          </label>
+        </div>
         <button type="submit">Post Your Service</button>
       </form>
     </div>
